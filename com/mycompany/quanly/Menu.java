690f04d6b640262461db2fa84e83feabbc2d4d71
TNTan203 <119738086+TNTan203@users.noreply.github.com>
2022-12-05 19:08:55 +0700

    Add files via upload

---
+++ com/mycompany/quanly/Menu.java
@@ -0,0 +1,66 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+
+/**
+ *
+ * @author ADMIN
+ */
+abstract class Menu {
+    protected String maMon;
+    protected String tenMon;
+    protected int giaMon;
+
+    public Menu(String maMon, String tenMon, int giaMon) {
+        this.maMon = maMon;
+        this.tenMon = tenMon.toUpperCase();
+        this.giaMon = giaMon;
+    }
+
+    /**
+     * @return the maMon
+     */
+    public String getMaMon() {
+        return maMon;
+    }
+
+    /**
+     * @param maMon the maMon to set
+     */
+    public void setMaMon(String maMon) {
+        this.maMon = maMon;
+    }
+
+    /**
+     * @return the tenMon
+     */
+    public String getTenMon() {
+        return tenMon;
+    }
+
+    /**
+     * @param tenMon the tenMon to set
+     */
+    public void setTenMon(String tenMon) {
+        this.tenMon = tenMon;
+    }
+
+    /**
+     * @return the giaMon
+     */
+    public int getGiaMon() {
+        return giaMon;
+    }
+
+    /**
+     * @param giaMon the giaMon to set
+     */
+    public void setGiaMon(int giaMon) {
+        this.giaMon = giaMon;
+    }
+
+    
+}
