8b55facc58adb155f3ccf47b867a9f8680427b41
TNTan203 <119738086+TNTan203@users.noreply.github.com>
2022-12-06 22:15:48 +0700

    Add files via upload

---
+++ ThucAn.java
@@ -0,0 +1,227 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ThucAn extends Menu {
+
+    private boolean anChay= true ;
+    private final String url = "C:\\Data\\ThucAn";
+    private File file = new File(getUrl());
+
+    public ThucAn(String maMon, String tenMon, int giaMon, boolean anChay) {
+        super(maMon, tenMon, giaMon);
+        this.anChay = anChay;
+    }
+
+    private static List<ThucAn> monAn = new ArrayList<>();
+
+    public void init() {
+        ThucAn ta = new ThucAn();
+        try ( Scanner c = new Scanner(file)) {
+            c.useDelimiter(",");
+            while (c.hasNext()) {
+                String maMon = c.next();
+                String tenMon = c.next();
+                int giaMon = c.nextInt();
+                boolean anChay = c.nextBoolean();
+                ThucAn n = new ThucAn(maMon, tenMon, giaMon, anChay);
+                getMonAn().add(n);
+                c.nextLine();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    //Them phan tu//
+    public void themThucAn() throws FileNotFoundException, IOException {
+        try ( FileWriter fileWriter = new FileWriter(getFile(), true)) {
+            try ( PrintWriter writer = new PrintWriter(fileWriter)) {
+                writer.print(this.getMaMon() + ",");
+                writer.print(this.getTenMon() + ",");
+                writer.print(Integer.toString(this.getGiaMon()) + ",");
+                writer.print(Boolean.toString(this.anChay) + ",");
+                writer.println();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        getMonAn().add(this);
+    }
+
+    //Phuong Thuc Ho Tro//
+    public void capNhat(String noiDung) throws IOException {
+        String[] a = noiDung.split(",");
+        for (int i = 0; i < a.length; i++) {
+            this.capNhat(a[i], i + 1);
+        }
+        rewriteFile();
+    }
+
+    public void capNhat(String noiDung, int viTriSua) throws IOException {
+        if (viTriSua == 1) {
+            this.setMaMon(noiDung.toUpperCase());
+        } else if (viTriSua == 2) {
+            this.setTenMon(noiDung.toUpperCase());
+        } else if (viTriSua == 3) {
+            this.setGiaMon(Integer.parseInt(noiDung));
+        } else {
+            this.anChay = Boolean.parseBoolean(noiDung);
+        }
+        rewriteFile();
+    }
+
+    //Phuong Thuc Ho Tro Xoa
+    public ThucAn timXoa(String maMon) {
+        ThucAn food = new ThucAn();
+        for (ThucAn s : getMonAn()) {
+            if (s.getMaMon().toLowerCase().equals(maMon.toLowerCase())) {
+                food = s;
+            }
+        }
+        return food;
+    }
+
+    public void xoaThucAn(String ma) throws IOException {
+        for (ThucAn s : getMonAn()) {
+            if (s.maMon.toLowerCase().equals(ma.toLowerCase())) {
+                System.out.println(s.maMon.toLowerCase().equals(ma.toLowerCase()));
+                System.out.println(s.maMon);
+                getMonAn().remove(s);
+                break;
+            }
+        }
+        rewriteFile();
+    }
+
+    public static List<ThucAn> timThucAn(String noiDung, int i) throws FileNotFoundException {
+        List<ThucAn> ta = new ArrayList<>();
+        for (ThucAn food : getMonAn()) {
+            if (i == 0) {
+                if (food.maMon.toLowerCase().contains(noiDung.toLowerCase())) {
+                    ta.add(food);
+                }
+            } else if (i == 1) {
+                if (food.tenMon.toLowerCase().contains(noiDung.toLowerCase())) {
+                    ta.add(food);
+                }
+            } else if (i == 2) {
+                if (food.giaMon >= Integer.parseInt(noiDung)) {
+                    ta.add(food);
+                }
+            } else {
+                if (Boolean.toString(food.anChay).toLowerCase().contains(noiDung.toLowerCase())) {
+                    ta.add(food);
+                }
+            }
+
+        }
+        return ta;
+    }
+
+    //Tao ma moi//
+    private String nextMaThucAn(String maMon) {
+        if (maMon == null) {
+            maMon = "100001";
+        } else {
+            int nextMa = (Integer.parseInt(maMon) + 1);
+            maMon = Integer.toString(nextMa);
+        }
+        return maMon;
+    }
+
+    private String docMaThucAn() throws FileNotFoundException {
+        String maMon = null;
+        File file = new File(this.url);
+        if (file.length() != 0) {
+            try ( Scanner scanner = new Scanner(file)) {
+                scanner.useDelimiter(",");
+                while (scanner.hasNextLine()) {
+                    maMon = scanner.next();
+                    scanner.nextLine();
+                }
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+        return maMon;
+    }
+
+    //Viet lai//
+    public void rewriteFile() throws IOException {
+        try ( FileWriter fileWriter = new FileWriter(file, false)) {
+            try ( PrintWriter writer = new PrintWriter(fileWriter)) {
+                for (ThucAn s : getMonAn()) {
+                    writer.print(s.getMaMon() + ",");
+                    writer.print(s.getTenMon() + ",");
+                    writer.print(Integer.toString(s.getGiaMon()) + ",");
+                    writer.print(Boolean.toString(s.anChay) + ",");
+                    writer.println();
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public ThucAn() {
+        this(null, null, 0, false);
+    }
+
+    /**
+     * @return the anChay
+     */
+    public boolean isAnChay() {
+        return anChay;
+    }
+
+    /**
+     * @param anChay the anChay to set
+     */
+    public void setAnChay(boolean anChay) {
+        this.anChay = anChay;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @return the file
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * @return the monAn
+     */
+    public static List<ThucAn> getMonAn() {
+        return monAn;
+    }
+
+    /**
+     * @param aMonAn the monAn to set
+     */
+    public static void setMonAn(List<ThucAn> aMonAn) {
+        monAn = aMonAn;
+    }
+}
---
+++ ThucUong.java
@@ -0,0 +1,222 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ *
+ * @author ADMIN
+ */
+public class ThucUong extends Menu {
+
+    private String hangSx;
+    private final String url = "C:\\Data\\ThucUong";
+    private File file = new File(getUrl());
+
+    public ThucUong(String maMon, String tenMon, int giaMon, String hangSx) {
+        super(maMon, tenMon, giaMon);
+        this.hangSx = hangSx;
+    }
+
+    private static List<ThucUong> monUong = new ArrayList<>();
+
+    public void init() {
+        ThucUong tu = new ThucUong();
+        try ( Scanner d = new Scanner("C:\\Data\\ThucUong.txt")) {
+            d.useDelimiter(",");
+            while (d.hasNext()) {
+                String maMon = d.next();
+                String tenMon = d.next();
+                int giaMon = d.nextInt();
+                String hangSx = d.next();
+                ThucUong m = new ThucUong(maMon, tenMon, giaMon, hangSx);
+                getMonUong().add(m);
+                d.nextLine();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    public void themThucUong() throws FileNotFoundException, IOException {
+        try ( FileWriter fileWriter = new FileWriter(getFile(), true)) {
+            try ( PrintWriter writer = new PrintWriter(fileWriter)) {
+                writer.print(this.getMaMon() + ",");
+                writer.print(this.getTenMon() + ",");
+                writer.print(Integer.toString(this.getGiaMon()) + ",");
+                writer.print(this.hangSx + ",");
+                writer.println();
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+        getMonUong().add(this);
+    }
+
+    public void capNhat(String noiDung) throws IOException {
+        String[] b = noiDung.split(",");
+        for (int i = 0; i < b.length; i++) {
+            this.capNhat(b[i], i + 1);
+        }
+        rewriteFile();
+    }
+
+    public void capNhat(String noiDung, int viTriSua) throws IOException {
+        if (viTriSua == 1) {
+            this.setMaMon(noiDung.toUpperCase());
+        } else if (viTriSua == 2) {
+            this.setTenMon(noiDung.toUpperCase());
+        } else if (viTriSua == 3) {
+            this.setGiaMon(Integer.parseInt(noiDung));
+        } else {
+            this.hangSx = (noiDung.toUpperCase());
+        }
+        rewriteFile();
+    }
+
+    //Phuong Thuc Ho Tro Xoa
+    public ThucUong timXoa(String maMon) {
+        ThucUong drink = new ThucUong();
+        for (ThucUong x : getMonUong()) {
+            if (x.getMaMon().toLowerCase().equals(maMon.toLowerCase())) {
+                drink = x;
+            }
+        }
+        return drink;
+    }
+
+    public void xoaThucUong(String ma) throws IOException {
+        for (ThucUong x : getMonUong()) {
+            if (x.maMon.toLowerCase().equals(ma.toLowerCase())) {
+                System.out.println(x.maMon.toLowerCase().equals(ma.toLowerCase()));
+                System.out.println(x.maMon);
+                getMonUong().remove(x);
+                break;
+            }
+        }
+        rewriteFile();
+    }
+
+    public List<ThucUong> timThucUong(String noiDung, int i) throws FileNotFoundException {
+        List<ThucUong> tu = new ArrayList<>();
+        for (ThucUong drink : getMonUong()) {
+            if (i == 0) {
+                if (drink.maMon.toLowerCase().contains(noiDung.toLowerCase())) {
+                    tu.add(drink);
+                }
+            } else if (i == 1) {
+                if (drink.tenMon.toLowerCase().contains(noiDung.toLowerCase())) {
+                    tu.add(drink);
+                }
+            } else if (i == 2) {
+                if (drink.giaMon >= Integer.parseInt(noiDung)) {
+                    tu.add(drink);
+                }
+            } else {
+                if (drink.hangSx.toLowerCase().contains(noiDung.toLowerCase())) {
+                    tu.add(drink);
+                }
+            }
+        }
+        return tu;
+    }
+
+    private String nextMaThucUong(String maMon) {
+        if (maMon == null) {
+            maMon = "200001";
+        } else {
+            int nextMa = (Integer.parseInt(maMon) + 1);
+            maMon = Integer.toString(nextMa);
+        }
+        return maMon;
+    }
+
+    private String docMaThucUong() throws FileNotFoundException {
+        String maMon = null;
+        File file = new File(this.url);
+        if (file.length() != 0) {
+            try ( Scanner scanner = new Scanner(file)) {
+                scanner.useDelimiter(",");
+                while (scanner.hasNextLine()) {
+                    maMon = scanner.next();
+                    scanner.nextLine();
+                }
+            } catch (Exception ex) {
+                System.out.println(ex);
+            }
+        }
+        return maMon;
+    }
+
+    public void rewriteFile() throws IOException {
+        try ( FileWriter fileWriter = new FileWriter(file, false)) {
+            try ( PrintWriter writer = new PrintWriter(fileWriter)) {
+                for (ThucUong x : getMonUong()) {
+                    writer.print(x.getMaMon() + ",");
+                    writer.print(x.getTenMon() + ",");
+                    writer.print(Integer.toString(x.getGiaMon()) + ",");
+                    writer.print(x.getHangSx() + ",");
+                    writer.println();
+                }
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+        }
+    }
+
+    ThucUong() {
+        this(null, null, 0, null);
+    }
+
+    /**
+     * @return the hangSx
+     */
+    public String getHangSx() {
+        return hangSx;
+    }
+
+    /**
+     * @param hangSx the hangSx to set
+     */
+    public void setHangSx(String hangSx) {
+        this.hangSx = hangSx;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @return the monUong
+     */
+    public List<ThucUong> getMonUong() {
+        return monUong;
+    }
+
+    /**
+     * @return the fileB
+     */
+    public File getFile() {
+        return file;
+    }
+
+    /**
+     * @param aMonUong the monUong to set
+     */
+    public static void setMonUong(List<ThucUong> aMonUong) {
+        monUong = aMonUong;
+    }
+}
